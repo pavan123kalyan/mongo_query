3211557e4676dd3b4a3eba7bae3d61a8f52bd387
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-04-18 14:11:39 +0530

    added

--- q1.js
+++ q1.js
@@ -4,6 +4,7 @@
 // show collections;
 // db.createCollection("products")  Creates a collection
 // show collections;
+
 //db.products.updateMany({},{$set:{price:1000}})
 // db.Product.renameCollection("fruits")  It change the existed name of collection.
 // db.users.countDocuments();  It counts the no.of data 
@@ -29,6 +30,31 @@
 // db.employees.find({},{_id:0,name:1,salary:1}).sort({salary:1}) sorts in ascending order.
 //
 //db.employees.find({"address.city":"nagole"});  It retrieve id and cities.
-
+//db.employees.find({"address.state":"Telangana"}) It retrieve all data related to Telangana
+// .
 // db.employees.find({skills:"Java"})  It retrieve data who have java skill.
 // db.employees.find().skip(1);  It skips 1st data.
+
+// db.users1.updateOne({name:"varun"},{$set:{location:"Hyderabad"}})  It adds new column location.
+//db.users1.updateMany({name:{$in:["arya","sailu"]}},{$set:{location:"Bangalore"}})
+
+//db.users1.find({name:"arya"})  Here {} this brackets mandatory.
+//db.users1.find({email:"vijay@gmail.com"},{_id:false,name:1})      It retrieve name with help of email.
+
+
+//db.users1.deleteOne({name:"arya"})    It deletes data related to arya.
+//db.employees.deleteMany({ department: "test" }); It deletes all data related to department as test.
+// db.users1.drop();   It deletes the entire data in users1.
+
+//db.employees.find({department:"CSE"})
+//db.employees.find({department:{$eq:"CSE"}})  --> Both are same,eq==equal
+//db.employees.find({salary:{$gt:50000}})    --> It retrieve data who has salary more than 50k,gt=greater than.
+
+// db.employees.find({salary:{$lt:100000}})   --> lt=less than.
+//db.employees.find({salary:{$lte:50000}})   --> lte=Less than equal to.
+//db.employees.find({salary:{$gte:50000}})   --> gte=greater than equal to.
+
+//db.employees.find({
+//     salary: { $gte: 50000 },
+//     department: "CSE",
+//   });
--- q5.js
+++ q5.js
@@ -2,35 +2,36 @@ db.users.deleteOne({ name: "test1" });
 
 db.employees.drop();
 
-db.employees.insertOne({
-  name: "John Smith",
-  email: "john@gmail.com",
-  address: { city: "Columbus", state: "OH" },
-  department: "IT",
-  salary: 1456,
-  score: [4, 1, 6, 4],
-  skills: ["Java", "Python"],
-  date: Date(),
-});
-
-db.employees.insertOne({
-    name: "Cathy George",
-    email: "cathy@gmail.com",
-    address: { city: "Jacksonville", state: "FL" },
-    department: "HR",
-    salary: 2500,
-    score: [6, 2, 6, 4],
-    skills: ["Java", "Python","Reactjs","MongoDB"],
-    date: Date(),
-  });
-
   db.employees.insertOne({
-    name: "Mike Covington",
-    email: "mike@gmail.com",
-    address: { city: "Columbus", state: "OH" },
-    department: "Admin",
-    salary: 1500,
-    score: [6, 4],
-    skills: ["Java", "Reactjs"],
-    date: Date(),
-  });
+        name:'siddhu',
+        email:'siddhu@gmail.com',
+        address:{city:"Dundigal",state:'TG'},
+        department:'IT',
+        salary:99999,
+        score:[6,2,6,4],
+      skills:["python","DSA","Reactjs","MongoDB"],
+      date:Date(),
+    });
+
+   db.employees.insertOne({
+       name:'sriram',
+         email:'sriram@gmail.com',
+         address:{city:"Nagole",state:'Telangana'},
+         department:'CSM',
+         salary:50000,
+         score:[9,6,7,4],
+         skills:["python","DSA","Reactjs","MongoDB","ML"],
+         date:Date()
+     })
+     
+     db.employees.insertOne({
+           name:'Hitesh',
+           email:'athi@gmail.com',
+           address:{city:"Nagole",state:'Telangana'},
+           department:'Admin',
+           salary:50000,
+           score:[9,6],
+           skills:["python","MongoDB"],
+           date:Date()
+       })
+  
