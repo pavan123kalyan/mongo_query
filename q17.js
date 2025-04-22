9c1e494389d8278ab0dbbdb42138cfdc6ac02045
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-04-22 12:25:27 +0530

    updated

--- q17.js
+++ q17.js
@@ -4,3 +4,28 @@ db.employees.aggregate([
     {$skip:3},
     {$limit:1}
 ])
+
+db.employees.aggregate([
+    {$group:{_id:"$department",total:{$sum:"$salary"}}}
+])
+
+db.employees.aggregate([
+    {$project:{_id:0,name:1,skills:1}},
+    ])
+
+db.employees.aggregate([
+{$project:{_id:0,name:1,skills:1}},
+{$unwind:"$skills"}
+])
+
+// --->unwind use is ,for each skills name will be going to print(it gets redundancy)
+
+    db.employees.aggregate([
+        {$project:{_id:0,
+            name:1,
+            department:1,
+            salaryStr:{$convert:{input:"$salary",to:"int"}},
+        },
+    },
+            {$group:{_id:"department",total:{$sum:"$salaryStr"}}},
+        ])
