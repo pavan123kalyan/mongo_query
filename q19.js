75aa020275d8ea715ec4342ecb2a42f869096280
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-04-22 14:39:22 +0530

    added

--- q19.js
+++ q19.js
@@ -21,5 +21,62 @@ db.posts.aggregate([
       $project: {_id: 0,title: 1,description: 1,"author.name":1}
     }
   ]);
+
+  db.authors.aggregate([
+    {
+      $lookup: {
+        from: "authors",
+        localField: "authorId",
+        foreignField: "_id",
+        as: "author"
+      }
+    },
+    {
+      $project: {_id: 0,title: 1,description: 1,"author.name":1}
+    }
+  ]);
   
-Title,Description,author
+
+  db.authors.aggregate([
+    {
+      $lookup: {
+        from: "posts",               // target collection (posts)
+        localField: "_id",           // author._id
+        foreignField: "authorId",    // posts.authorId
+        as: "posts"                  // array of posts
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        name: 1,
+        "posts.title": 1,
+        "posts.description": 1
+      }
+    }
+  ]);
+
+  
+db.authors.aggregate([
+    {$lookup:{
+        from:"posts",
+        localField:"_id",
+        foreignField:"authorId",
+        as:"posts"
+    }},
+    {$unwind:"$posts"}
+
+])
+
+
+
+db.posts.insertOne({
+    title:"title 75",
+    desc: "desc 75",
+    authorId:ObjectId('68075aff10180e78792f45df')
+})
+
+db.authors.insertOne({
+    name:"John",
+    email:"john@email.com"
+})
